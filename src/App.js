2cb30b78187c9f2141575d01c85c9e16028e7c41
Tucker <leachtucker@gmail.com>
2021-01-14 14:58:16 -0800

    Set up some basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import logo from './assets/logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} alt="Logo" className="logo" />
+        <h1 className="title">HowTo</h1>
       </header>
+      <div className="view-container">
+        <nav>
+          <ul>
+            <li>Dashboard</li>
+            <li>Post</li>
+            <li>Edit</li>
+            <li>Logout</li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
